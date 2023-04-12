f87913756b1dff166c03f87064a828919be39a64
hungnv198 <127464412+hungnv198@users.noreply.github.com>
2023-04-12 17:24:58 +0700

    up nào

---
+++ test.js
@@ -0,0 +1,23 @@
+const app = {
+    currentIndex : 0,
+    users: [
+        {
+            name:"hùng",
+            age:20
+        }
+    ],
+    defineProperties: function () {
+        Object.defineProperty(this, "currentUser", {
+            get: function () {
+                return this.users[this.currentIndex];
+            },
+        });
+    },
+    loadUser: function(){
+        console.log(this.currentUser.name);
+    },
+    start: function(){
+        this.loadUser();
+    }
+}
+app.start();
